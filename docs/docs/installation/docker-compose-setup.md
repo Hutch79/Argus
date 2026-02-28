fb9208b22370e7f66d23bacc1a40c668bd410583
Hutch79 <git@hutch79.ch>
2026-02-28 16:23:06 +0100

    feat(docs): add initial documentation files and styles
    Added various markdown files for documentation, including installation,
    getting started, and markdown features. Also included CSS styles for
    layout and presentation.

---
+++ docs/docs/installation/docker-compose-setup.md
@@ -0,0 +1,14 @@
+---
+title: Docker Compose Setup
+description: Install Argus with docker compose
+sidebar_position: 1
+---
+
+## Pre Requisits
+- Docker Compose must be installed
+
+:::tip[My tip]
+
+Docker Compose is the easiest way to get started ^^
+
+:::
---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,96 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'Argus',
+  tagline: ' A multi eyed monitoring solution ',
+  favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://argus.hutch79.ch',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Hutch79', // Usually your GitHub org/user name.
+  projectName: 'Argus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/hutch79/argus/tree/main/docs/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Argus',
+      logo: {
+        alt: 'Argus Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        // {
+        //   type: 'docsVersionDropdown',
+        // },
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/hutch79/argus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `© ${new Date().getFullYear()} Hutch79 — Licensed under <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
